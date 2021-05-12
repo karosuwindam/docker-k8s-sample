9ef74a2536dc7acd3f3b4edb46552d1d3b9f7ef0
karosuwindam <karosuwindam@gmail.com>
2021-05-12 03:57:10 +0100

    0.2 version up

--- gpio_go/golang/main.go
+++ gpio_go/golang/main.go
@@ -8,11 +8,14 @@ import (
 
 var (
 	co2limit   = int(1000)
+	avglimit = int(60)
 	co2chktime = time.Duration(1000) //ms
 )
 
 type Ckdata struct {
 	data int
+	avg int
+	avgdate []int
 }
 
 func (t *Ckdata) check_data() bool {
@@ -23,7 +26,16 @@ func (t *Ckdata) check_data() bool {
 		if data.Type == "co2" {
 			co2data, _ := strconv.Atoi(data.Data)
 			t.data = co2data
-			if co2data > co2limit {
+			if len(t.avgdate) > avglimit{
+				t.avgdate = t.avgdate[1:avglimit]
+			}
+			t.avgdate = append(t.avgdate,co2data)
+			tmp := 0
+			for _,data := range t.avgdate{
+				tmp += data
+			}
+			t.avg = tmp/len(t.avgdate)
+			if t.avg > co2limit {
 				flag = true
 			}
 		}
@@ -55,18 +67,20 @@ func main() {
 	}()
 	gpiodata.OutPin[0] = LED_OFF
 	var co2data Ckdata
+	co2data.avgdate = []int{}
 	go func() {
 		count := 1
 		for {
+			starttime := time.Now()
 			if co2data.check_data() {
 				gpiodata.OutPin[0] = LED_ON
 				if count != 0 {
-					fmt.Println("LED", LED_ON, co2data.data)
+					fmt.Println(starttime,"LED", LED_ON, co2data.data,co2data.avg)
 				}
 				count = 0
 			} else if count > 5 {
 				if (count % 10) == 0 {
-					fmt.Println("LED", LED_OFF, co2data.data)
+					fmt.Println(starttime,"LED", LED_OFF, co2data.data,co2data.avg)
 				}
 				gpiodata.OutPin[0] = LED_OFF
 				count++
@@ -74,7 +88,7 @@ func main() {
 				gpiodata.OutPin[0] = LED_TOGGLE
 				count++
 			}
-			time.Sleep(time.Millisecond * co2chktime)
+			time.Sleep(time.Millisecond * co2chktime )
 		}
 	}()
 	for {
