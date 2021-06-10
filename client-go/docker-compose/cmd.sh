bdec07b2e066378014bef13fbf66a31c606a6ba6
karosuwindam <karosuwindam@gmail.com>
2021-06-11 07:53:27 +0900

    ok

--- client-go/docker-compose/cmd.sh
+++ client-go/docker-compose/cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 name="bookserver2:31000/karosu/client-go"
 version="0.0.1"
-docker build -t $name:$version -f ./app/Dockerfile_arm ./app
+docker build -t $name:$version -f ./Dockerfile_arm ./
 docker push $name:$version
