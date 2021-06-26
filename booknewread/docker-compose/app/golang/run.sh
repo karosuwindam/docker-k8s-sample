8cb48eff55906477f0797456b504643d0b07d6f4
karosuwindam <karosuwindam@gmail.com>
2021-06-26 09:14:30 +0900

    update

--- booknewread/docker-compose/app/golang/run.sh
+++ booknewread/docker-compose/app/golang/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-if [ -z "$(ls $directory)" ]; then
+directory=bookmark
+
+if [ ! -d $directory ]; then
+    echo "create bookmark dir"
+    mkdir $directory
+fi
+
+if [ -z "$(ls bookmark)" ]; then
     echo "copy bookmarks.html"
     cp bookmarks.html bookmark
 fi
