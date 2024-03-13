b1808880b5807196f30ab4817e353ecdb2409c66
karosuwindam <karosuwindam@gmail.com>
2024-03-13 10:10:35 +0900

    add marge

--- booknewread/docker-compose/app/golang-v4/main.go
+++ booknewread/docker-compose/app/golang-v4/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"book-newread/config"
+	"book-newread/loop"
+	"book-newread/webserver"
+)
 
+func Init() error {
+	if err := config.Init(); err != nil {
+		return err
+	}
+	if err := loop.Init(); err != nil {
+		return err
+	}
+	if err := webserver.Init(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Start() error {
+	return nil
+}
+
+func Stop() {
+
+}
+
+func main() {
+	if err := Init(); err != nil {
+		panic(err)
+	}
+	if err := Start(); err != nil {
+		panic(err)
+	}
+	Stop()
 }
