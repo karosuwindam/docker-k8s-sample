db9781e3526d2f685762b2097cbe7971089cfb7f
karosuwindam <karosuwindam@gmail.com>
2024-03-15 10:04:44 +0900

    update data

--- booknewread/docker-compose/app/golang-v4/webserver/api/readdatastore/readDataStore.gen.go
+++ booknewread/docker-compose/app/golang-v4/webserver/api/readdatastore/readDataStore.gen.go
@@ -3,6 +3,7 @@ package readdatastore
 import "net/http"
 
 func Init(url string, mux *http.ServeMux) error {
-	mux.HandleFunc("GET "+url, readNewBook)
+	mux.HandleFunc("GET "+url+"/book/:page", readNewBook)
+	mux.HandleFunc("GET "+url+"/nobel", readNewNobel)
 	return nil
 }
--- booknewread/docker-compose/app/golang-v4/webserver/api/readdatastore/readNewNobel.go
+++ booknewread/docker-compose/app/golang-v4/webserver/api/readdatastore/readNewNobel.go
@@ -1,6 +1,21 @@
 package readdatastore
 
-import "net/http"
+import (
+	"book-newread/loop/datastore"
+	"book-newread/loop/novelchack"
+	"encoding/json"
+	"net/http"
+)
 
 // データストアからWeb小説のデータを読み取る
-func readNewNobel(w http.ResponseWriter, r *http.Request) {}
+func readNewNobel(w http.ResponseWriter, r *http.Request) {
+	var data novelchack.List
+	if err := datastore.Read(&data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
