0d9c2a5326e5323f3f9d038ffc254290d20c06f3
karosuwindam <karosuwindam@gmail.com>
2024-03-15 14:03:44 +0900

    add http test code

--- booknewread/docker-compose/app/golang-v4/webserver/api/hello/hello.gen.go
+++ booknewread/docker-compose/app/golang-v4/webserver/api/hello/hello.gen.go
@@ -4,6 +4,9 @@ import "net/http"
 
 func Init(url string, mux *http.ServeMux) error {
 	mux.HandleFunc("GET "+url, HelloWeb)
+	if url == "/" {
+		url = ""
+	}
 	mux.HandleFunc("GET "+url+"/{id}", HelloId)
 	return nil
 }
